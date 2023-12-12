7509a292ac9146053051ac4ec6e371815e263a90
Chad Smith <chad.e.smith@outlook.com>
2023-12-11 22:47:04 -0600

    REFACTOR code cleanup for readability

--- routes/weapon.router.js
+++ routes/weapon.router.js
@@ -3,15 +3,26 @@ const pool = require('../modules/pool');
 const router = express.Router();
 const weaponGenerator = require('../modules/weapon-generator');
 
-/**
- * GET '/'
- * Endpoint returns weapons matching the search query.
- */
 router.get('/', async (req, res) => {
+  const queryString = `
+    SELECT *
+    FROM weapons
+    LIMIT 100;
+  `;
+  try
+  {
+    const dbResponse = await pool.query(queryString);
+    dbResponse.rowCount > 0 ? res.status(200).send(dbResponse.rows) : res.status(404).send('Could not locate the requested weapons. Please try again.');
+  } catch {
+    res.status(500).send('There was a problem locating the requested weapons. Please try again.');
+  }
+});
+
+router.post('/search', async (req, res) => {
   const hitPointSearch = req.body.hitPointSearch || { min: 50, max: 200 };
   const durabilitySearch = req.body.durabilitySearch || { min: 50, max: 100 };
 
-  let queryString = `
+  const queryString = `
     SELECT *
     FROM weapons
     WHERE name = ANY($1)
@@ -19,7 +30,8 @@ router.get('/', async (req, res) => {
       AND (hit_points BETWEEN $3 AND $4
       AND durability BETWEEN $5 AND $6);
   `;
-  try {
+  try
+  {
     const dbResponse = await pool.query(queryString, [
       req.body.nameList,
       req.body.typeList,
@@ -28,9 +40,7 @@ router.get('/', async (req, res) => {
       durabilitySearch.min,
       durabilitySearch.max,
     ]);
-    dbResponse.rowCount > 0 ?
-      res.status(200).send(dbResponse.rows) :
-      res.status(404).send('No matching records found. Please modify your search and try again.');
+    dbResponse.rowCount > 0 ? res.status(200).send(dbResponse.rows) : res.status(404).send('No matching records found. Please modify your search and try again.');
   } catch {
     res.status(500).send(`Could not find weapons at this time. Please try again.`);
   }
@@ -50,76 +60,52 @@ router.get('/', async (req, res) => {
  * }
  */
 router.get('/new', async (req, res) => {
-  try {
-    const weapon = weaponGenerator();
-    console.log('Weapon', weapon);
+  const weapon = weaponGenerator();
+  try
+  {
     res.send(weaponGenerator()).status(200);
-  } catch (error) {
-    res
-      .status(500)
-      .send('Could not generate a weapon at this time. Please try again.');
+  } catch (error)
+  {
+    res.status(500).send('Could not generate a weapon at this time. Please try again.');
   }
 });
 
-/**
- * GET `/:weaponId`
- * Endpoint returns a specific weapon by `id`.
- *
- * {
- *   "display_name": "Mace of Shame",
- *   "hit_points": 99,
- *   "is_repairable": true,
- *   "id": 1003
- * }
- */
 router.get('/:weaponId', async (req, res) => {
-  try {
-    const weaponId = req.params.weaponId;
-    const queryString = `
-    SELECT display_name, hit_points, is_repairable, durability, id
+  const weaponId = req.params.weaponId;
+  const queryString = `
+    SELECT weapon_type, display_name, hit_points, is_repairable, durability, id
     FROM weapons
     WHERE id = $1
     LIMIT 1`;
+  try
+  {
     const weapon = await pool.query(queryString, [weaponId]);
-    if (weapon.rowCount > 0) {
+    if (weapon.rowCount > 0)
+    {
       res.status(200).send(weapon.rows[0]);
     }
-    if (weapon.rowCount === 0) {
+    if (weapon.rowCount === 0)
+    {
       res.status(404).send(`Weapon not found.`);
     }
-  } catch (error) {
-    res
-      .status(500)
-      .send(`Could not get your weapon at this time. Please, try again.`);
+  } catch (error)
+  {
+    res.status(500).send(`Could not get your weapon at this time. Please, try again.`);
   }
   res.end();
 });
 
-/**
- * POST `/`
- * Endpoint saves a weapon. Payload expected as follows:
- * {
- *  "weapon_type": "Mace",
- *  "name": "Honor",
- *  "hit_points": 147,
- *  "durability": 99,
- *  "is_repairable": false,
- *  "display_name": "Mace of Honor"
- * }
- *
- * Returns the newly created weapon.
- */
 router.post('/', async (req, res) => {
-  try {
-    const weapon = req.body;
-    const queryString = `
+  const weapon = req.body;
+  const queryString = `
       WITH newWeapon as (
         INSERT INTO weapons (weapon_type, name, hit_points, durability, is_repairable, display_name) values ($1, $2, $3, $4, $5, $6)
         RETURNING *
       )
       SELECT * FROM newWeapon;
     `;
-
+  try
+  {
     const dbUpdate = await pool.query(queryString, [
       weapon.weapon_type,
       weapon.name,
@@ -129,78 +115,67 @@ router.post('/', async (req, res) => {
       weapon.display_name,
     ]);
     res.status(201).send(dbUpdate.rows[0]);
-  } catch (error) {
+  } catch (error)
+  {
     res.status(500).send(`Could not save the weapon. Please try again.`);
   }
 });
 
 router.put('/:weaponId', async (req, res) => {
-  console.group('PUT Weapon');
   const weaponId = req.params.weaponId;
-  console.info('Weapon ID from params', weaponId);
-
-  try {
-    console.info('Weapon id matches', weaponId == req.body.id);
-    if (!weaponId || weaponId != req.body.id) {
-      res.status(400).send('Please provide a valid weapon ID.');
-    }
-    const weaponQuery = `
+  const weaponQuery = `
       SELECT *
       FROM weapons
       WHERE id = $1;
     `;
-    const weaponList = await pool.query(weaponQuery, [weaponId]);
-    console.log('Weapon List', weaponList.rows);
-    if (weaponList.rowCount !== 1) {
-      res
-        .status(404)
-        .send(`Could update weapon with ID of ${weaponId}. Please try again.`);
-    }
-
-    const updateWeaponQuery = `
+  const updateWeaponQuery = `
       UPDATE weapons
       SET durability = $1
       WHERE id = $2
       RETURNING *;
     `;
-
+  try
+  {
+    if (!weaponId || weaponId != req.body.id)
+    {
+      res.status(400).send('Please provide a valid weapon ID.');
+    }
+    const weaponList = await pool.query(weaponQuery, [weaponId]);
+    if (weaponList.rowCount !== 1)
+    {
+      res.status(404).send(`Could update weapon with ID of ${weaponId}. Please try again.`);
+    }
     const dbUpdate = await pool.query(updateWeaponQuery, [
       req.body.durability,
       weaponId,
     ]);
-    console.info('Update return from the DB', dbUpdate);
-    if (dbUpdate.rowCount === 1) {
+    if (dbUpdate.rowCount === 1)
+    {
       res.status(200).send(dbUpdate.rows[0]);
     }
-  } catch (error) {
-    console.error('Error from update', error);
-    res
-      .status(500)
-      .send(
-        `Could not update weapon with ID ${weaponId} at this time. Please try again.`
-      );
+  } catch {
+    res.status(500).send(`Could not update weapon with ID ${weaponId} at this time. Please try again.`);
   }
-  console.groupEnd();
 });
 
 router.delete('/:weaponId', async (req, res) => {
-  try {
-    const weaponId = req.params.weaponId;
-    if (!weaponId) {
-      res.status(400).send('Please provide a valid weapon ID.');
-    }
-    const queryString = `
+  const weaponId = req.params.weaponId;
+  const queryString = `
       DELETE FROM weapons
       WHERE id = $1;
     `;
+  try {
+    if (!weaponId)
+    {
+      res.status(400).send('Please provide a valid weapon ID.');
+    }
     const dbUpdate = await pool.query(queryString, [weaponId]);
-    if (dbUpdate.rowCount === 1) {
+    if (dbUpdate.rowCount === 1)
+    {
       res.status(200).send('Weapon is deleted.');
     }
-  } catch (error) {
-    res
-      .status(500)
-      .send('Could not delete the weapon at this time. Please try again.');
+  } catch {
+    res.status(500).send('Could not delete the weapon at this time. Please try again.');
   }
 });
 
--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cors = require('cors');
 require('dotenv').config();
 const app = express();
 
@@ -6,6 +7,7 @@ const weaponRouter = require('./routes/weapon.router');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cors());
 
 app.use('/api/weapon', weaponRouter);
 
